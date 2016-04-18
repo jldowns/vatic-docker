5af393df01093fe60877aacf6a5384856ecd37a9
jldowns <justinlawrencedowns@gmail.com>
2016-04-18 09:36:10 -0700

    added video

--- annotate.sh
+++ annotate.sh
@@ -4,8 +4,8 @@ ANNOTATION_SCRIPT=$1
 
 JOB=$(\
 docker run -ditP -v "$PWD/data":/root/vatic/data \
-                -v "$PWD/annotation_scripts":/root/vatic/ascripts \
-                jldowns/vatic-docker /bin/bash -C /root/vatic/ascripts/$ANNOTATION_SCRIPT \
+                 -v "$PWD/annotation_scripts":/root/vatic/ascripts \
+                 jldowns/vatic-docker /bin/bash -C /root/vatic/ascripts/$ANNOTATION_SCRIPT \
     )
 
 PORT=$(docker port $JOB 80 | awk -F: '{ print $2 }')
